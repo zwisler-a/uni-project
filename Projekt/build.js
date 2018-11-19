7f3d6b778c6d0f1c67a5f560895f0615470fedce
Maurice Eisenblätter <code@ingrim4.me>
2018-11-19 01:10:36 +0100

    chore: Removed PKG from bundler

--- Projekt/build.js
+++ Projekt/build.js
@@ -1,10 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
 const childProcess = require('child_process');
-const { exec } = require('pkg');
 
-const { clientPath, serverPath, buildPath,
-		targetPlatforms, targetArch, production } = require('./config');
+const { clientPath, serverPath, buildPath, production } = require('./config');
 const buildPackage = path.join(buildPath, 'package.json');
 
 const getNpmPath = (basePath) => {
@@ -32,33 +30,5 @@ const prepareDirectory = () => {
 	fs.copySync(path.join(serverPath, 'src'), buildPath);
 };
 
-const prepareServer = () => {
-	childProcess.execFileSync(
-		getNpmPath(buildPath),
-		[ 'install', '--production' ],
-		{ env: process.env, cwd: buildPath }
-	);
-};
-
-const bundle = async () => {
-	const dist = path.join(buildPath, 'dist');
-	fs.mkdirsSync(dist);
-	process.chdir(dist);
-
-	for(platform of targetPlatforms) {
-		for(arch of targetArch) {
-			await exec([ buildPath, '--target', `latest-${platform}-${arch}`, '--output', `latest-${platform}-${arch}` ]);
-		}
-	}
-
-	const target = path.join(__dirname, '..', 'dist');
-	fs.copySync(dist, target);
-};
-
 buildClient();
 prepareDirectory();
-prepareServer();
-bundle().catch(error => {
-	console.error(error);
-	process.exit(1);
-});
--- Projekt/config.js
+++ Projekt/config.js
@@ -4,7 +4,5 @@ module.exports = {
 	clientPath: path.resolve(__dirname, 'client'),
 	serverPath: path.resolve(__dirname, 'server'),
 	buildPath: path.resolve(__dirname, 'dist'),
-	targetPlatforms: [ 'win', 'linux' ],
-	targetArch : [ 'x64', 'x86' ],
 	production: true
 };
