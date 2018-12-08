b011fc1307c914ab95b9c8fc0bc0f1dbe19aa273
Zwisler <alex-z-@web.de>
2018-12-08 17:02:59 +0100

    chore: added some documentation

--- Projekt/client/src/app/shell/navigation/navigation-group/navigation-group.component.ts
+++ Projekt/client/src/app/shell/navigation/navigation-group/navigation-group.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { NavigationGroup } from '../types/navigation-group.interface';
 
+/**
+ * Component to display the actual NavigationGroup
+ */
 @Component({
     selector: 'app-navigation-group',
     templateUrl: './navigation-group.component.html',
     encapsulation: ViewEncapsulation.None
 })
 export class NavigationGroupComponent implements OnInit {
+    /** Group to display */
     @Input()
     group: NavigationGroup;
 
+    /** If it should display the navigation in smaller form */
     @Input()
     collapsed = false;
 
--- Projekt/client/src/app/shell/navigation/sidenav-control-button/sidenav-control-button.component.ts
+++ Projekt/client/src/app/shell/navigation/sidenav-control-button/sidenav-control-button.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { SidenavState } from '../types/sidenav-state.enum';
 import { SidenavMode } from '../types/sidenav-mode.enum';
-
+/**
+ * Component to decide in which state the sidenav should transition.
+ * Also properly indicates whats its going todo once clicked
+ */
 @Component({
     selector: 'app-sidenav-control-button',
     templateUrl: './sidenav-control-button.component.html',
     styleUrls: ['./sidenav-control-button.component.scss']
 })
 export class SidenavControlButtonComponent implements OnInit {
+    /** State of the sidenav */
     @Input()
     state: SidenavState;
-
+    /** Mode of the sidenav */
     @Input()
     mode: SidenavMode;
 
+    // One of these gets triggered if the button is clicked
     @Output()
     expand = new EventEmitter<void>();
 
@@ -27,6 +32,7 @@ export class SidenavControlButtonComponent implements OnInit {
 
     ngOnInit() {}
 
+    /** Handles the decision making what should happen when the button is pressen */
     do() {
         if (this.state === SidenavState.expanded) {
             this.mode === SidenavMode.over
@@ -37,6 +43,7 @@ export class SidenavControlButtonComponent implements OnInit {
         }
     }
 
+    /** Decides which icon should be displayed */
     get icon() {
         return this.state !== SidenavState.expanded
             ? 'keyboard_arrow_right'
