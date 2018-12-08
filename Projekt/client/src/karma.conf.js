a8235acc84d81b599c3eacba812b4f6958a1826e
Zwisler <alex-z-@web.de>
2018-12-08 14:52:52 +0100

    test: removed phantomjs

--- Projekt/client/src/karma.conf.js
+++ Projekt/client/src/karma.conf.js
@@ -12,7 +12,6 @@ module.exports = function(config) {
             require("karma-jasmine"),
             require("karma-junit-reporter"),
             require("karma-chrome-launcher"),
-            require("karma-phantomjs-launcher"),
             require("karma-jasmine-html-reporter"),
             require("karma-coverage-istanbul-reporter"),
             require("@angular-devkit/build-angular/plugins/karma")
