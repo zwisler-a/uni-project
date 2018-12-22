c777e0d9ba9df836d2a51068cbd33514cd5ce4f0
zwisler <alex-z-@web.de>
2018-12-22 16:06:20 +0100

    fix: table setup race condition

--- Projekt/server/src/database/controller.ts
+++ Projekt/server/src/database/controller.ts
@@ -1,9 +1,11 @@
 import { Pool } from '../../types/mariadb';
-import { CREATE_TABLE_USER, USER_CREATE } from './querys';
+import { CREATE_TABLE_USER, USER_CREATE, USER_GET } from './querys';
 
-export function initializeTables(pool: Pool) {
-    CREATE_TABLE_USER(pool);
+export async function initializeTables(pool: Pool) {
+    await CREATE_TABLE_USER(pool);
 
     // Add a mock user as long as there is no other way to add users ('username', 'password')
-    USER_CREATE(pool, [ 'username', '$2b$10$sFut8f1wXaMisJ750uiGbOD8UefoIZLLad5a66M7f/YMV5okNUgEC' ]).catch(err => null);
+    if (!(await USER_GET(pool, 'username')).pop()) {
+        await USER_CREATE(pool, [ 'username', '$2b$10$sFut8f1wXaMisJ750uiGbOD8UefoIZLLad5a66M7f/YMV5okNUgEC' ]);
+    }
 }
