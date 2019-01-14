6fbaa758aa90bb561283be04f5a444f9d04f856f
Maurice Eisenblätter <code@ingrim4.me>
2019-01-14 17:14:31 +0100

    feat: JUnit for backend

--- Projekt/server/src/database/query.ts
+++ Projekt/server/src/database/query.ts
@@ -1,14 +1,30 @@
 import { Pool, Connection, Resultsets } from '../../types/mariadb';
 
+/**
+ * Very simple abstraction for queries
+ * @author Maurice
+ */
 export abstract class Query {
+
+    /** Current datbase pool instance */
     pool: Pool;
 
+    /**
+     * Creates a new query
+     * @param pool current database pool instance
+     */
     constructor(pool: Pool) {
         this.pool = pool;
 
+        // Bind function so we have the query's instance in `this`
         this.error = this.error.bind(this);
     }
 
+    /**
+     * Helper method for Promise.catch()
+     * @param error the reason Promise.catch got called
+     * @throws the given error to the next handler
+     */
     protected error(error: any): any {
         // Remove sensitive data (parameters)
         error.message = error.message.split('\n', 1).shift();
@@ -21,47 +37,109 @@ export abstract class Query {
     }
 }
 
+/**
+ * A helper class for static (don't change at runtime) sql queries
+ * @author Maurice
+ */
 export class StaticQuery<T extends Resultsets> extends Query {
+
+    /** The static sql query */
     sql: string;
 
+    /**
+     * Creates a new static query
+     * @param pool current database pool instance
+     * @param sql static sql query
+     */
     constructor(pool: Pool, sql: string) {
         super(pool);
         this.sql = sql;
     }
 
+    /**
+     * Executes the query on the pool
+     * @param values values to be used for the query (prepared statement)
+     * @returns the query's resultset
+     */
     async execute(values?: any[] | any): Promise<T> {
         return this.pool.query(this.sql, values).catch(this.error) as Promise<T>;
     }
 
+    /**
+     * Executes the query on a specify connection
+     * @param connection connection to execute on
+     * @param values values to be used for the query (prepared statement)
+     * @returns the query's resultset
+     */
     async executeConnection(connection: Connection, values?: any[] | any): Promise<T> {
         return connection.query(this.sql, values).catch(this.error) as Promise<T>;
     }
 }
 
+/**
+ * Function that generates a valid sql query at runtime
+ * @author Maurice
+ */
 export interface DynamicQueryBuilder {
+    /**
+     * Generates a sql query string at runtime
+     * @param structure parameters to be used to generate the query
+     * @returns a valid sql query
+     */
     (structure: any): string;
 }
 
+/**
+ * A helper class for dynamic (at runtime generated) sql queries
+ * @author Maurice
+ */
 export class DynamicQuery<T extends Resultsets> extends Query {
+
+    /** Sql query build */
     builder: DynamicQueryBuilder;
 
+    /**
+     * Creates a new dynamic query
+     * @param pool current database pool instance
+     * @param sql this query's sql builder
+     */
     constructor(pool: Pool, builder: DynamicQueryBuilder) {
         super(pool);
         this.builder = builder;
     }
 
+    /**
+     * Helper method for Promise.catch() which does the same thing as {@link Query.error} and also attaches the generated sql
+     * @param error the reason Promise.catch got called
+     * @param sql the generated sql
+     * @throws the given error to the next handler
+     */
     private errorDynamic(error: any, sql: string) {
         // Set generate sql
         error.sql = sql;
 
+        // Call Query.error for further error improvements
         this.error(error);
     }
 
+    /**
+     * Executes the query on the pool
+     * @param structure parameters used to generate the query
+     * @param values values to be used for the query (prepared statement)
+     * @returns the query's resultset
+     */
     async execute(structure: any, values?: any[] | any): Promise<T> {
         const sql = this.builder(structure);
         return this.pool.query(sql, values).catch(error => this.errorDynamic(error, sql)) as Promise<T>;
     }
 
+    /**
+     * Executes the query on a specify connection
+     * @param connection connection to execute on
+     * @param structure parameters used to generate the query
+     * @param values values to be used for the query (prepared statement)
+     * @returns the query's resultset
+     */
     async executeConnection(connection: Connection, structure: any, values?: any[] | any): Promise<T> {
         const sql = this.builder(structure);
         return connection.query(sql, values).catch(error => this.errorDynamic(error, sql)) as Promise<T>;
