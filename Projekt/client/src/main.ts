81753b815e7c26c6ecd209a146c6c665dfc82804
Zwisler <alex-z-@web.de>
2019-01-15 09:46:48 +0100

    fix: layout bugs

--- Projekt/client/src/main.ts
+++ Projekt/client/src/main.ts
@@ -6,8 +6,10 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
+    console.log = () => {};
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
