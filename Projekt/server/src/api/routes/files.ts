c2a5dec15bb370e54a70adf4efd88460258f6f1b
rs92vegi <robin.seidel@hypservice.de>
2019-03-01 10:47:12 +0100

    routes..

--- Projekt/server/src/api/routes/files.ts
+++ Projekt/server/src/api/routes/files.ts
@@ -3,9 +3,12 @@ import { verifyJsonWebToken } from '../controllers/authentication';
 import { fileInformation, fileUpload, fileDownload, fileUpdate, fileDelete } from '../controllers/files';
 
 
-export const itemsRoute: Router = Router();
-itemsRoute.get('/:type/:id', verifyJsonWebToken, fileInformation);
-itemsRoute.get('/:type/:id/download', verifyJsonWebToken, fileDownload);
-itemsRoute.post('/:type/:id', verifyJsonWebToken, fileUpload);
-itemsRoute.patch('/:type/:id', verifyJsonWebToken, fileUpdate);
-itemsRoute.delete('/:type/:id', verifyJsonWebToken, fileDelete);
+export const filesRoute: Router = Router();
+// get information which files exists for a specific item
+filesRoute.get('/:type/:id', verifyJsonWebToken, fileInformation);
+// get files for an item 
+filesRoute.get('/:type/:id/download', verifyJsonWebToken, fileDownload);
+// upload one new file for an item 
+filesRoute.post('/:type/:id', verifyJsonWebToken, fileUpload);
+filesRoute.patch('/:type/:id', verifyJsonWebToken, fileUpdate);
+filesRoute.delete('/:type/:id', verifyJsonWebToken, fileDelete);
