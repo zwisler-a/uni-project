1bd9751214b7aea2e3152153bf11a7dfc7315398
rs92vegi <robin.seidel@hypservice.de>
2019-03-01 11:39:23 +0100

    added multer for file upload

--- Projekt/server/src/api/controllers/files.ts
+++ Projekt/server/src/api/controllers/files.ts
@@ -2,6 +2,23 @@ import { Response, Request, NextFunction } from 'express';
 
 import { Field, ITEM } from '../models/item';
 
+const multer = require('multer');
+
+// save file to disk in specific folder 
+// path is saved in db, in table item(?)
+
+
+var storage = multer.diskStorage({
+     destination: function(req, file, cb){
+          cb(null, 'uploads')
+      },
+       filename: function(req, file, cb){
+          cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+    var upload = multer({ storage: storage}); 
+
+
 
 export async function fileInformation(req: Request, res: Response, next: NextFunction){
     try {
@@ -13,6 +30,11 @@ export async function fileInformation(req: Request, res: Response, next: NextFun
 
 export async function fileUpload(req: Request, res: Response, next: NextFunction) {
     try {
+        const file = req.file
+        if(!file){
+            const error = new Error('No files choosen!')
+        }
+        res.send(file)
         res.status(200);
     } catch (error) {
         next(error);
