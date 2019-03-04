718b1aa2935343785e07fdcb35f562bc213ef5ed
rs92vegi <robin.seidel@hypservice.de>
2019-03-04 18:28:12 +0100

    upload..

--- Projekt/server/src/api/controllers/files.ts
+++ Projekt/server/src/api/controllers/files.ts
@@ -1,23 +1,17 @@
 import { Response, Request, NextFunction } from 'express';
-
 import { Field, ITEM } from '../models/item';
-
-const multer = require('multer');
-
-// save file to disk in specific folder 
-// path is saved in db, in table item(?)
-
+import * as multer from 'multer'
 
 var storage = multer.diskStorage({
-     destination: function(req, file, cb){
-          cb(null, 'uploads')
-      },
-       filename: function(req, file, cb){
-          cb(null, file.fieldname + '-' + Date.now())
+    destination: function(req, file, callback){
+        callback(null, './uploads');
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}`);
     }
-  })
-    var upload = multer({ storage: storage}); 
+});
 
+var upload = multer.default({storage: storage});
 
 
 export async function fileInformation(req: Request, res: Response, next: NextFunction){
@@ -30,16 +24,21 @@ export async function fileInformation(req: Request, res: Response, next: NextFun
 
 export async function fileUpload(req: Request, res: Response, next: NextFunction) {
     try {
-        const file = req.file
-        if(!file){
-            const error = new Error('No files choosen!')
-        }
-        res.send(file)
+        const companyId: number = req.params.companyId;
+        const typeId: number = req.params.type;
+        const id: number = req.params.id;
+
+        const folderName = `${companyId}${typeId}${id}`
+
+        upload.single('file');
+
+        res.send(req.file)
         res.status(200);
     } catch (error) {
         next(error);
     }
 }
+
 
 export async function fileDownload(req: Request, res: Response, next: NextFunction) {
     try {
